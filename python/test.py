9d29ee6ad348079fb5a10cc3bc705de012860d6b
KCsup <kcsup05@gmail.com>
2024-03-15 13:49:48 -0400

    Fixed python formatting.

--- python/test.py
+++ python/test.py
@@ -1,29 +1,9 @@
-def main():
-    print(get_prime_string(3))
+import main
 
 
-def get_prime_string(n):
-    primeStore = [2]
-    primeConcat = "2"
-
-    for i in range(n + 5):
-        nextPrime = primeStore[i] + 1
-
-        primeFound = False
-        while not primeFound:
-            for c in range(2, nextPrime):
-                if nextPrime % c == 0:
-                    nextPrime += 1
-                    break
-                elif c == nextPrime - 1:
-                    primeFound = True
-                    break
-
-        primeStore.append(nextPrime)
-        primeConcat += str(nextPrime)
-
-    return primeConcat[n:n+5]
+def main_entrance():
+    print(main.get_prime_string(3))
 
 
 if __name__ == "__main__":
-    main()
+    main_entrance()
